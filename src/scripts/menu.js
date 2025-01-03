5c9ede5131a926345be906a8c4c3bb38953b7a88
John De Costa <thefoxboxman@users.noreply.github.com>
2025-01-03 10:23:03 +1000

    built components and deploy to pages

---
+++ src/scripts/menu.js
@@ -0,0 +1,7 @@
+// OPEN AND CLOSE HAMBURGER
+
+
+      document.querySelector(".hamburger")?.addEventListener("click", () => {
+        document.querySelector(".nav-links")?.classList.toggle("expanded");
+      });
+  
